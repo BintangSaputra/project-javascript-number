d8bbdb13fd03ba07081ebe95a511ee01add23765
BintangSaputra <bintangcbs26@gmail.com>
2019-04-05 11:26:48 +0700

    add-javascript-number-practice

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(25);
+console.log(300);
+console.log(3.25);
+console.log("Hello World");
+let age = 52;
+
+let name = "Hello World";
+
+console.log(`His name ${name} and his age ${age}`);
